6ea00cc7cb92a896e232113feb2ebe188fde8a7c
HidanRP <maxime.cuadro@laplateforme.io>
2024-12-02 10:15:24 +0100

    Après avoir importé la bibliothèque Tkinter et fait les fenêtres je commences à faire une grille de 9 boutons afin de faire les cases du morpion

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter
+
+def draw_grid():
+
+# Faire la fenêtre du jeu
+root = tkinter.Tk()
+
+# Personnalisation de la fenêtre
+root.title("TicTacToe")
+root.minsize(500, 500)
+root.mainloop()
